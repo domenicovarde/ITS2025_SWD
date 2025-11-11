14e376d9fbb08bc51fe54ce20fef483ec43a0e2f
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2025-11-11 09:32:02 +0100

    agg

---
+++ spring-angular/formicolangular/src/app/prodotto/prodotto.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prodotto',
+  imports: [],
+  templateUrl: './prodotto.html',
+  styleUrl: './prodotto.css',
+})
+export class Prodotto {
+
+}
